a707c0ee432f50742b524f30bb3942068cd09e46
Rubens94 <ruben9_1994@hotmail.com>
2021-07-21 19:55:44 -0500

    Sanitizando campos de usuario

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -16,13 +16,13 @@ const router = Router();
 
 // Crear usuario
 router.post('/',[
-    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-    check('apellidos', 'Los apellidos son obligatorios').not().isEmpty(),
-    check('correo', 'El correo es obligatorio').not().isEmpty(),
+    check('nombre', 'El nombre es obligatorio').not().isEmpty().escape().trim(),
+    check('apellidos', 'Los apellidos son obligatorios').not().isEmpty().escape().trim(),
+    check('correo', 'El correo es obligatorio').not().isEmpty().escape().trim(),
     check('correo', 'El correo no es válido').isEmail(),
-    check('password', 'El password es obligatorio').not().isEmpty(),
+    check('password', 'El password es obligatorio').not().isEmpty().escape().trim(),
     check('password', 'El password debe de tener más de 6 carácteres').isLength({ min: 6 }),
-    check('confirmar', 'La confirmación del password es obligatorio').not().isEmpty(),
+    check('confirmar', 'La confirmación del password es obligatorio').not().isEmpty().escape().trim(),
     validarRegistro
 ],  crearUsuario);
 
